cfb5eaad720fe92d76a3c99977294c25d25426e6
Zachary Job <twentyfive11001@gmail.com>
2017-01-02 20:24:39 -0500

    Add files via upload

--- SRC_FS_MERGE/create.sh
+++ SRC_FS_MERGE/create.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 #Zachary Job
 
-LIB_LOC=./EXT_LIBS/BUILT_LIBS
+LIB_LOC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/EXT_LIBS/BUILT_LIBS"
 libs_exist=true
 
-if [ ! -f $LIB_LOC/libssh2_s.so ]; then
+if [ ! -e "$LIB_LOC/libssh2_s.so" ]; then
+	echo "LIBSSH2 NOT FOUND"
 	libs_exist=false
 fi
-if [ ! -f $LIB_LOC/libz_s.so ]; then
+if [ ! -e $LIB_LOC/libz_s.so ]; then
+	echo "LIBZ NOT FOUND"
 	libs_exist=false
 fi
-if [ ! -f $LIB_LOC/libaes_s.a ]; then
+if [ ! -e $LIB_LOC/liboaes_s.a ]; then
+	echo "LIB OAES NOT FOUND"
 	libs_exist=false
 fi
-if [ ! -f $LIB_LOC/libcrypto_s.a ]; then
+if [ ! -e $LIB_LOC/libcrypto_s.a ]; then
+	echo "LIBCRYPTO NOT FOUND"
 	libs_exist=false
 fi
 
+#lazzzzzy
 if [ $libs_exist == false ]; then
-	echo "A library was not found, building..."
+	echo "A library was not found, building..."	
 	cd ./EXT_LIBS
 	./build.sh
 	cd ..
 fi
-
+	
 make
